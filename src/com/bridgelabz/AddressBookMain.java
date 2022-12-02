bd8d604265405ecd24e5549383ebdeb310984277
9663461085 <ratansollapure16310@gmail.com>
2022-12-02 23:05:55 +0530

    Added:Welcome Meassge To Addressbook program

---
+++ src/com/bridgelabz/AddressBookMain.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+/***
+ * 
+ * @author Admin
+ *
+ */
+public class AddressBookMain {
+	public static void main(String[] args) {
+		System.out.println("Welcome to AddressBook Program");
+	}
+}
