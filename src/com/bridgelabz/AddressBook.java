f7372aac9a60cdbd594349158299888a417ca59c
9663461085 <ratansollapure16310@gmail.com>
2022-12-09 20:14:37 +0530

    Added:Multiple Address book By using HashMap

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -1,6 +1,7 @@
 package com.bridgelabz;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
@@ -11,69 +12,77 @@ import java.util.Scanner;
  */
 public class AddressBook {
 	List<Contact> contactList = new ArrayList<>();
-
-	Scanner input = new Scanner(System.in);
+	HashMap<String, AddressBook> addressBooks = new HashMap<>();
+	Scanner inputContact = new Scanner(System.in);
 	Contact newPerson = new Contact();
 
 	/*
 	 * Created Method To add new Contact details
 	 */
 	public void getContact() {
-		System.out.println("Enter the details of contact =>");
-		System.out.println("Enter your First_Name");
-		newPerson.setFirst_Name(input.next());
-		System.out.println("Enter your Last_Name");
-		newPerson.setLast_Name(input.next());
-		System.out.println("Enter your Address");
-		newPerson.setAddress(input.next());
-		System.out.println("Enter your City ");
-		newPerson.setCity(input.next());
-		System.out.println("Enter your State");
-		newPerson.setState(input.next());
-		System.out.println("Enter your Zip code");
-		newPerson.setZip(input.nextInt());
-		System.out.println("Enter your Phone_Number");
-		newPerson.setPhone_Number(input.next());
-		System.out.println("Enter your EmailID");
-		newPerson.setEmail(input.next());
-		contactList.add(newPerson);
-		System.out.println();
-		System.out.println("Contact added");
+		System.out.print("Enter the number of contact you want to save:");
+		int numberOfContact = inputContact.nextInt();
+		for (int i = 1; i <= numberOfContact; i++) {
+			System.out.println("Enter the details of contact =>");
+			Contact contact = new Contact();
+			System.out.print("Enter first name:");
+			contact.setFirst_Name(inputContact.next());
+			System.out.print("Enter last name:");
+			contact.setLast_Name(inputContact.next());
+			System.out.print("Enter address:");
+			contact.setAddress(inputContact.next());
+			System.out.print("Enter city:");
+			contact.setCity(inputContact.next());
+			System.out.print("Enter state:");
+			contact.setState(inputContact.next());
+			System.out.print("Enter zipcode:");
+			contact.setZip(inputContact.nextInt());
+			System.out.print("Enter phone number:");
+			contact.setPhone_Number(inputContact.next());
+			System.out.print("Enter email:");
+			contact.setEmail(inputContact.next());
+			contactList.add(contact);
+			System.out.println();
+			System.out.println("Contact added");
+		}
+
 	}
-/**
- * Created method to edit contact details
- */
+
+	/**
+	 * Created method to edit contact details
+	 */
 	public void editContact() {
 		System.out.println("Enter contact's First Name you want to edit: ");
-		String searchName = input.next();
+		String searchName = inputContact.next();
 		for (Contact newPerson : contactList) {
 			if (newPerson.getFirst_Name().equals(searchName)) {
 				System.out.print("Enter updated name:");
-				newPerson.setFirst_Name(input.next());
+				newPerson.setFirst_Name(inputContact.next());
 				System.out.println("Enter your Last_Name");
-				newPerson.setLast_Name(input.next());
+				newPerson.setLast_Name(inputContact.next());
 				System.out.println("Enter your Address");
-				newPerson.setAddress(input.next());
+				newPerson.setAddress(inputContact.next());
 				System.out.println("Enter your City ");
-				newPerson.setCity(input.next());
+				newPerson.setCity(inputContact.next());
 				System.out.println("Enter your State");
-				newPerson.setState(input.next());
+				newPerson.setState(inputContact.next());
 				System.out.println("Enter your Zip code");
-				newPerson.setZip(input.nextInt());
+				newPerson.setZip(inputContact.nextInt());
 				System.out.println("Enter your Phone_Number");
-				newPerson.setPhone_Number(input.next());
+				newPerson.setPhone_Number(inputContact.next());
 				System.out.println("Enter your EmailID");
 			} else {
 				System.out.println("Contact not found with this name ");
 			}
 		}
 	}
-/*
- * Created method to delete contact
- */
+
+	/*
+	 * Created method to delete contact
+	 */
 	public void deleteContact() {
 		System.out.print("Enter contact's First Name you want to delete:");
-		String searchName = input.next();
+		String searchName = inputContact.next();
 		for (Contact newPerson : contactList) {
 			if (newPerson.getFirst_Name().equals(searchName)) {
 				contactList.remove(newPerson);
--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -13,22 +13,45 @@ public class AddressBookMain {
 	 * @param args
 	 */
 	/*
-	 * Created Method to Choose user to add,delete,eidt,display Contacts
+	 * Created Method to Choose user to add,adding Multiple
+	 * addressBook,delete,eidt,display Contacts
 	 */
 	void openContact() {
 		int choice;
 		AddressBook addressBook = new AddressBook();
+		AddressBook book1 = new AddressBook();
+		AddressBook book2 = new AddressBook();
+		AddressBook book3 = new AddressBook();
+		addressBook.addressBooks.put("AddressBook1", addressBook);
+		addressBook.addressBooks.put("AddressBook2", book1);
+		addressBook.addressBooks.put("AddressBook3", book2);
+		addressBook.addressBooks.put("AddressBook4", book3);
 		do {
 			Scanner inputChoice = new Scanner(System.in);
 			System.out.println(
 					"Enter your choice\n1: Add Contact\t2: Edit Contact\n3: Display Contact\t4: Delete Contact");
 			choice = inputChoice.nextInt();
 			/*
-			 * Used switch case for choice
+			 * Used switch case for choice1
 			 */
 			switch (choice) {
 			case 1:
-				addressBook.getContact();
+				System.out.println("Enter:\n1:Address Book\t2:Address Book1\n3:Address Book2\t4:Address Book3");
+				int chooseAddressBook = inputChoice.nextInt();
+				/*
+				 * Used If-else condition to check the Result.
+				 */
+				if (chooseAddressBook == 1) {
+					addressBook.getContact();
+				} else if (chooseAddressBook == 2) {
+					book1.getContact();
+				} else if (chooseAddressBook == 3) {
+					book2.getContact();
+				} else if (chooseAddressBook == 4) {
+					book3.getContact();
+				} else {
+					System.out.println("Option not found");
+				}
 				break;
 			case 2:
 				addressBook.editContact();
